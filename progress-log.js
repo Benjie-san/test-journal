3fb87c9da2c424cae07cf72e14b0a2243f5979e4
Benjie-San <haisesasaki56@gmail.com>
2024-03-02 15:21:48 +0800

    added logs

---
+++ progress-log.js
@@ -0,0 +1,53 @@
+//||========================||//
+//||   Record of Progress   ||//
+//||========================||//
+
+// To do for today
+/*
+
+*/
+
+//==========================//
+
+// Features to add
+/*
+    -
+
+    -
+
+    -
+*/
+
+//---------------------------//
+
+// Added features
+/*
+    - Theme setting(colors and font)
+    
+    - Notification System
+
+    - Sorting by Month
+*/
+
+//==========================//
+
+// Bugs to Fix
+/*
+    -
+
+    -
+
+    -
+*/
+
+//---------------------------//
+
+
+// Fixed Issues
+/*
+    - Saving / Auto Save issues
+
+    -
+
+    -  
+*/
