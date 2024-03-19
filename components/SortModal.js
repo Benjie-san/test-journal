c887b8fe413d89ccd4d9424219264bbbe5ec1269
Benjie-San <haisesasaki56@gmail.com>
2024-03-19 21:45:28 +0800

    made some ui changes and progress

--- components/SortModal.js
+++ components/SortModal.js
@@ -4,9 +4,6 @@ import Modal from "react-native-modal";
 import { useTheme } from 'react-native-paper'; 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Checkbox } from 'react-native-paper';
-import { FontAwesome } from '@expo/vector-icons';
-import { Ionicons } from '@expo/vector-icons';
-import { Entypo } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 import { Foundation } from '@expo/vector-icons';
@@ -14,14 +11,25 @@ import { Fontisto } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import styles from '../styles/passageStyle';
 const Tab = createMaterialTopTabNavigator();
+import { useNavigation } from '@react-navigation/native';
 
-export default function SortModal({visible, handleModal, }) {
+export default function SortModal({visible, handleModal }) {
 
     const theme = useTheme();
 	const [currentFilter, setCurrentFilter] = useState("All");
 	const [checked, setChecked] = useState(true);
 	const [selected, setSelected] = useState(true);
-	const [currentSort, setCurrentSort] = useState("By Month")
+	const [currentSort, setCurrentSort] = useState("By Month");
+
+	const [height, setHeight] = useState(250);
+
+	const handleheight = (item) =>{
+		setHeight(item)
+	}
+
+	const [iconName, setIconName] = useState("arrowdown");
+
+
     const toggleModal = () =>{
         handleModal(!visible)
     }
@@ -31,13 +39,15 @@ export default function SortModal({visible, handleModal, }) {
 			backgroundColor: theme.colors.primary, 
 			borderTopColor: theme.colors.borderColor, 
 			borderTopWidth: 1,
-			height: '100%',
 			padding: 10, 
-			margin: 0
+			margin: 0,
+			height: '100%',
+			paddingTop: 20,
 		}
 	}
 
 	const RenderFilter = () =>{
+		const navigation = useNavigation();
 
 		const handleFilter = (name) =>{
 			setChecked(true);
@@ -58,6 +68,17 @@ export default function SortModal({visible, handleModal, }) {
 				<Text style={{color: theme.colors.textColor, fontSize: theme.fonts.fontSize}} >{name}</Text>
 			</TouchableOpacity>
 		);
+
+		useEffect(() => {
+			const unsubscribe = navigation.addListener('tabPress', (e) => {
+				// let splitTarget = e.target.split("-")
+				// console.log("First Screen Index:", splitTarget[0]);
+				handleheight(250);
+			});
+		
+			return unsubscribe;
+		}, [navigation]);
+		
 	
 		return(
 			<View style={renderStyles.style}>
@@ -65,18 +86,23 @@ export default function SortModal({visible, handleModal, }) {
 					<FilterItem name="Journal" />
 					<FilterItem name="Sermon Notes" />
 					<FilterItem name="OPM Reflection" />
-
-
 			</View>
 		);
 	}
 	
 	const RenderSort = () =>{
-
+		const navigation = useNavigation();
 
 		const handleSortItem = (name) =>{
-			setSelected(!selected)
-			setCurrentSort(name)
+			setSelected(true);
+			setCurrentSort(name);
+			if(selected && name == currentSort){
+				if(iconName == "arrowdown"){
+					setIconName("arrowup");
+				}else{
+					setIconName("arrowdown");
+				}
+			}
 		}
 
 		const SortItem = ({ name,  }) => (
@@ -84,10 +110,21 @@ export default function SortModal({visible, handleModal, }) {
 				onPress={ ()=>handleSortItem(name) }
 				style={{flexDirection: 'row', alignItems: 'center', padding: 10, gap: 10}}
 			>
-				<AntDesign name={selected ? 'arrowdown' : 'arrowup'} size={24} color={ name == currentSort ? theme.colors.textColor : theme.colors.altTextColor} />
+				<AntDesign name={iconName} size={24} color={ name == currentSort ? theme.colors.textColor : theme.colors.altTextColor} />
 				<Text style={{color: theme.colors.textColor, fontSize: theme.fonts.fontSize}} >{name}</Text>
 			</TouchableOpacity>
 		);
+		useEffect(() => {
+			const unsubscribe = navigation.addListener('tabPress', (e) => {
+				// let splitTarget = e.target.split("-")
+				// console.log("Second Screen Index:", splitTarget[0]);
+				handleheight(250);
+
+			});
+	
+		return unsubscribe;
+		}, [navigation]);
+		
 
 		return(
 			<View style={renderStyles.style}>
@@ -99,9 +136,9 @@ export default function SortModal({visible, handleModal, }) {
 	}
 	
 	const RenderDisplay = () => {
-
+		const navigation = useNavigation();
 		const handleDisplayItem = () =>{
-
+			console.log("change layout")
 		}
 
 		const DisplayItem = ({ name, icon  }) => (
@@ -114,17 +151,25 @@ export default function SortModal({visible, handleModal, }) {
 			</TouchableOpacity>
 		);
 
+		useEffect(() => {
+			const unsubscribe = navigation.addListener('tabPress', (e) => {
+				handleheight(300)
+			});
+	
+		return unsubscribe;
+		}, [navigation]);
+
 		return(
 			<View style={renderStyles.style}>
 				<DisplayItem name="List" icon={ <Foundation name="list" size={30} color={theme.colors.textColor} /> }/>
 				<DisplayItem name="Details" icon={<FontAwesome5 name="equals" size={24} color={theme.colors.textColor} />} />
 				<DisplayItem name="Grid" icon={<Fontisto name="nav-icon-grid" size={24} color={theme.colors.textColor} />} />
-				<DisplayItem name="Large Grid" icon={<MaterialCommunityIcons name="view-grid" size={24} color={theme.colors.textColor}  />} />
-				
-
+				<DisplayItem name="Large Grid" icon={<MaterialCommunityIcons name="view-grid" size={24} color={theme.colors.textColor}  />} />	
 			</View>
 		);
 	}
+
+
 
 
     return (
@@ -144,9 +189,10 @@ export default function SortModal({visible, handleModal, }) {
             hideModalContentWhileAnimating
 			style={[styles.modal,]}
 		>
-			<View style={[styles.modalContent, {backgroundColor: theme.colors.primary, height: 300}]}>
+			<View style={[styles.modalContent, {backgroundColor: theme.colors.primary, height: height}]}>
 				<Tab.Navigator
 					screenOptions={{
+						style:{backgroundColor: theme.colors.primary,},
 						tabBarLabelStyle:{color: theme.colors.textColor,},
 						tabBarStyle:{
 							width: '100%',
@@ -161,9 +207,10 @@ export default function SortModal({visible, handleModal, }) {
 						swipeEnabled: false,
 					}}
 				>
-					<Tab.Screen headerTitle="Filter" name="filter" component={RenderFilter} />
+					<Tab.Screen headerTitle="Filter" name="filter" component={RenderFilter}/>
 					<Tab.Screen headerTitle="Sort" name="sort" component={RenderSort} />
-					<Tab.Screen headerTitle="Display" name="display" component={RenderDisplay} />
+					<Tab.Screen headerTitle="Display" name="display" component={RenderDisplay}/ >
+				
 				</Tab.Navigator>
 			</View>
 
